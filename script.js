d3e13ce78beea0900f215645ac6fb095b8d630b0
Alfredo <alfie.deadmarsh@icloud.com>
2026-08-12 20:16:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function sayHello() {
+    alert("fuck you!");
+}
+function Hello() {
+    link = "https://www.youtube.com/watch?v=QINu5v9rATo";
+    window.open(link, "_blank");
+}
+function Hello2() {
+    link = "https://www.youtube.com/watch?v=Aq5WXmQQooo";
+    window.open(link, "_blank");
+} 
